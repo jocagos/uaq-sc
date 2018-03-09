0746fdcf6fba1d58bf79da3e7d83d0db98400f7d
Jose Luis Gallegos <jose.gallegos@cimat.mx>
2018-03-09 01:13:03 -0600

    Fixed solution - Burger Time?

--- uva/11661.c
+++ uva/11661.c
@@ -1,33 +1,40 @@
-// UVa 11661 - Burger Time?
+/* UVa 11661 - Burger Time? */
 #include <stdio.h>
 
 int main( void ){
-    int l = -1, i, idx, min;
+    int l = -1, i, d, min;
     char f, c;
+    scanf("%d ", &l);
     while( l ){
-        scanf("%d ", &l);
-        idx = f = c = 0;
-        min = l+1;
+        d = 0;
+        f = '.';
+        min = l;
         for( i = 0; i < l; ++i ){
             scanf("%c", &c);
-            if( c == '.' || min == 0 ) continue;
             if( c == 'z' || c == 'Z' ){
                 min = 0;
                 scanf("%*s");
                 break;
             }
-            else if( ( c == 'R' || c == 'D' ) && !f ){
-                f = c;
-                idx = i;
+            if( c == 'R' ){
+                if( f == 'D' )
+                    if( d < min )
+                        min = d;
+                    d = 1;
+                    f = 'R';
+                    continue;
             }
-            else if( (c == 'R' || c == 'D' ) && c == f ) idx = i;
-            else if( ( c == 'R' || c == 'D' ) && f != c ){
-                min = ( i - idx < min ? i - idx : min );
-                f = c;
-                idx = i;
+            if( c == 'D' ){
+                if( f == 'R' )
+                    if( d < min ) min = d;
+                d = 1;
+                f = 'D';
+                continue;
             }
+            d++;
         }
         if( l ) printf("%d\n", min);
+        scanf("%d ", &l);
     }
     return 0;
 }
