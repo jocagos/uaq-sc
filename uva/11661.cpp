439e4d3d019caaf607c161e3e15db3a144ffe9a4
Jose Luis Gallegos <jose.gallegos@cimat.mx>
2018-03-10 20:28:11 -0600

    Burger Time - C++ - Working

--- uva/11661.cpp
+++ uva/11661.cpp
@@ -4,11 +4,40 @@
 using namespace std;
 
 int main(){
-    int l, r, d;
+    int l = -1, d, min;
+    char c;
     string s;
-    while( cin >> l ){
+    while( l ){
+        cin >> l;
+        min = l;
+        d = 0;
+        c = '.';
         if( !l ) break;
-        
+        cin >> s;
+        for( auto it = s.begin(); it != s.end(); it++ ){
+            if( *it == 'Z' ){
+                min = 0;
+                break;
+            }
+            if( *it == 'R'){
+                if( c == 'D' )
+                    if( d < min )
+                        min = d;
+                d = 1;
+                c = 'R';
+                continue;
+            }
+            if( *it == 'D'){
+                if( c == 'R' )
+                    if( d < min )
+                        min = d;
+                d = 1;
+                c = 'D';
+                continue;
+            }
+            d++;
+        }
+        cout << min << endl;
     }
     return 0;
 }
